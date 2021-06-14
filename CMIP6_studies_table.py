d9f467e9954be20a0c83440ef86203dfdfa69e83
Jesus Fernandez <Jesus.Fernandez@unican.es>
2021-06-14 14:06:11 +0200

    Fix table headers

--- CMIP6_studies_table.py
+++ CMIP6_studies_table.py
@@ -95,7 +95,7 @@ for i,key in enumerate(keys):
     searchkey = key if key[-6:-3] != 'ssp' else key[0:-7]
     thefile = [x.file for x in alldata if x.key==searchkey]
     thefile = '' if not thefile else thefile[0]
-    keys[i] = f'=Hyperlink("https://github.com/jesusff/cmip6-for-cordex/blob/performance_tables/{thefile}"; {key})'
+    keys[i] = f'"=Hyperlink(""https://github.com/jesusff/cmip6-for-cordex/blob/performance_tables/{thefile}"", ""{key}"")"'
 lines[1] = ','.join(keys)
 fp = open('CMIP6_studies_table.csv', 'w')
 fp.writelines([lines[0]]+lines[3:5]+lines[1:3]+lines[5:])
