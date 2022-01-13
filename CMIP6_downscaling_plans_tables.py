ae1f56bdfca111154f742a386f605d69d5922d68
Jesus Fernandez <Jesus.Fernandez@unican.es>
2022-01-13 13:31:05 +0100

    Update downscaling plans and tables / Add color to tables

--- CMIP6_downscaling_plans_tables.py
+++ CMIP6_downscaling_plans_tables.py
@@ -7,7 +7,6 @@ plans = pd.read_csv('CMIP6_downscaling_plans.csv')
 
 domains = sorted(list(set(plans.domain)))
 
-
 f = open(f'CMIP6_downscaling_plans_tables.html','w')
 f.write(f'''<!DOCTYPE html>
 <html><head>
@@ -16,6 +15,10 @@ body {{ padding-bottom: 600px; }}
 tr:hover {{background-color:#f5f5f5;}}
 th, td {{text-align: center; padding: 3px;}}
 table {{border-collapse: collapse;}}
+span.planned {{color: red}}
+span.running {{color: blue}}
+span.completed {{color: black}}
+span.published {{color: black; font-weight: bold}}
 a {{color: DodgerBlue}}
 a:link {{ text-decoration: none; }}
 a:visited {{ text-decoration: none; }}
@@ -24,17 +27,26 @@ a:active {{ text-decoration: underline;}}
 </style>
 </head><body>
 <h1> CORDEX-CMIP6 downscaling plans summary tables</h1>
-<p style="font-size: smaller; text-align: right;">(Version: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")})</p>
-<p style="font-size: smaller; text-align: justify;">
-Simulation status according to CORDEX-CMIP6 downscaling plans reported by the groups and collected in <a href="https://github.com/jesusff/cmip6-for-cordex/blob/main/CMIP6_downscaling_plans.csv">CMIP6_downscaling_plans.csv</a>. Check that file for further details or to contribute/update simulations.
+<p style="text-align: right;">(Version: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")})</p>
+<p style="text-align: justify;">
+Simulation status according to CORDEX-CMIP6 downscaling plans reported by the groups and collected in <a href="https://github.com/jesusff/cmip6-for-cordex/blob/main/CMIP6_downscaling_plans.csv">CMIP6_downscaling_plans.csv</a>. Check that file for further details or to contribute/update simulations. Colours indicate status as:
+<span class="planned">planned</span>
+<span class="running">running</span>
+<span class="completed">completed</span>
+<span class="published">published</span>
+<p style="text-align:left">
 ''')
+[f.write(f'<a href="#{dom}">{dom}</a> | ') for dom in domains]
 d1 = dict(selector=".level0", props=[('min-width', '150px')])
 for domain in domains:
-  f.write(f'<h2>{domain}</h2>')
-  f.write(plans[(plans.domain == domain) & (plans.experiment == 'historical')].pivot_table(
+  dom_plans = plans[(plans.domain == domain) & (plans.experiment.isin(['historical','ssp119','ssp126','ssp245','ssp370','ssp585']))]
+  dom_plans = dom_plans.assign(htmlstatus=pd.Series('<span class="' + dom_plans.status + '">' + dom_plans.experiment + '</span>', index=dom_plans.index))
+#  dom_plans['htmlstatus'] = ('<span class="' + dom_plans.status + '">' + dom_plans.experiment + '</span>').values
+  f.write(f'<h2 id="{domain}">{domain}</h2>')
+  f.write(dom_plans.pivot_table(
     index = ('driving_model', 'ensemble'),
     columns = 'rcm_name',
-    values = 'status',
+    values = 'htmlstatus',
     aggfunc = lambda x: ' '.join(x.dropna())
   ).style
      .set_properties(**{'font-size':'8pt', 'border':'1px lightgrey solid !important'})
