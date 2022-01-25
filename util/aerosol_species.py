26cf8c6770124222cd3c0bef736c680b81c83dc4
Jesus Fernandez <Jesus.Fernandez@unican.es>
2022-01-25 17:49:41 +0100

    Add aerosol species as other criteria
    Also:
    
     - Add util/aerosol_species.csv output
     - Sort members naturally in the aerosol species tables

--- util/aerosol_species.py
+++ util/aerosol_species.py
@@ -28,7 +28,7 @@ df.drop(
   axis = 'columns', inplace = True
 )
 df.drop_duplicates(inplace = True)
-#df.sort_values(['institution', 'model', 'ensemble', 'experiment', 'variable'])
+df.sort_values(['institution', 'model', 'ensemble', 'experiment', 'variable'])
 
 f = open(f'aerosol_species.html','w')
 f.write(f'''<!DOCTYPE html>
@@ -52,17 +52,17 @@ a:active {{ text-decoration: underline;}}
 <h1> CMIP6 ESGF Aerosol variables summary</h1>
 <p style="text-align: right;">(Version: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")})</p>
 <p style="text-align: justify;">
-
 This refers to the availability of od550* variables (e.g. so4 is aod550so4)
 ''')
 d1 = dict(selector=".level0", props=[('min-width', '100px')])
-dom_df_matrix = df.pivot_table(
+table = df.pivot_table(
   index = ('model', 'ensemble'),
   columns = 'experiment',
   values = 'variable',
   aggfunc = lambda x: ' '.join(x.dropna())
 )
-f.write(dom_df_matrix.style
+table = table.reindex(ns.natsorted(table.index))
+f.write(table.style
    .set_properties(**{'font-size':'8pt', 'border':'1px lightgrey solid !important'})
    .set_table_styles([d1,{
       'selector': 'th',
@@ -74,3 +74,27 @@ f.write(dom_df_matrix.style
 )
 f.write('</body></html>')
 f.close()
+
+table.to_csv('aerosol_species.csv', index_label = ['model', 'run'])
+
+print('''- key: Aer. species
+  doi: [pers. comm., Jesus Fernandez]
+  metric:
+    name: aer_species
+    long_name: Aerosol species for which AOD available at ESGF
+    units: categorical
+    variables: od550bb, od550bc, od550dust, od550no3, od550oa, od550so4, od550ss, od550so4so, aerasymbnd, aeroptbnd, aerssabnd
+    comment: >
+      Data extracted from ESGF using 
+      https://github.com/jesusff/cmip6-for-cordex/blob/main/util/aerosol_species.py
+      which feeds from
+      https://github.com/jesusff/cmip6-for-cordex/blob/main/CMIP6_for_CORDEX.py
+    best: bb, bc, dust, no3, oa, so4, ss, so4so, aerasymbnd, aeroptbnd, aerssabnd
+  type: other
+  spatial_scope: special
+  temporal_scope: Annual
+  data_source: author
+  data:''')
+t370 = table.ssp370.dropna().reset_index()
+for item in t370.iterrows():
+  print('    %s_%s: %s' % (item[1][0], item[1][1], item[1][2].replace('od550','')))
