75303930ec845cea4dadb9a8e90e032342b19e54
Jesus Fernandez <Jesus.Fernandez@unican.es>
2022-01-26 18:59:11 +0100

    Add initial support for experiments in the downscaling plans

---
+++ CORDEX_CMIP6_experiments.py
@@ -0,0 +1,97 @@
+import datetime
+import pandas as pd
+import yaml
+
+collapse_institutions = True
+
+plans = pd.read_csv('CMIP6_downscaling_plans.csv', na_filter=False)
+domains = sorted(list(set(plans.domain)))
+
+with open('CORDEX_CMIP6_experiments.yaml') as fp:
+  config = yaml.load(fp, Loader=yaml.FullLoader)
+
+f = open(f'CORDEX_CMIP6_experiments.html','w')
+f.write(f'''<!DOCTYPE html>
+<html><head>
+<style>
+body {{ padding-bottom: 600px; }}
+tr:hover {{background-color:#f5f5f5;}}
+th, td {{text-align: center; padding: 3px;}}
+table {{border-collapse: collapse;}}
+span.planned {{color: #FF9999}}
+span.running {{color: #009900}}
+span.completed {{color: black; font-weight: bold}}
+span.published {{color: #3399FF; font-weight: bold}}
+a {{color: DodgerBlue}}
+a:link {{ text-decoration: none; }}
+a:visited {{ text-decoration: none; }}
+a:hover {{ text-decoration: underline; }}
+a:active {{ text-decoration: underline;}}
+</style>
+</head><body>
+<h1> CORDEX-CMIP6 experiment summary tables</h1>
+<p style="text-align: right;">(Version: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")})</p>
+<p style="text-align: justify;">
+Simulation status for different experiments within domains, as collected from the CORDEX-CMIP6 downscaling plans reported by the groups in <a href="https://github.com/jesusff/cmip6-for-cordex/blob/main/CMIP6_downscaling_plans.csv">CMIP6_downscaling_plans.csv</a>. Check that file for further details. Experiment descriptions are provided at <a href="https://github.com/jesusff/cmip6-for-cordex/blob/experiment-tags/CORDEX_CMIP6_experiments.yaml">CORDEX_CMIP6_experiments.yaml</a>.
+To contribute/update simulations use this <a href="https://docs.google.com/document/d/1Jy53yvB9SDOiWcwKRJc_HpWVgmjxZhy-qVviHl6ymDM/edit?usp=sharing">Google doc</a>.
+''')
+d1 = dict(selector=".level0", props=[('min-width', '130px')])
+for domain in domains:
+  dom_plans = plans[plans.domain == domain]
+  tags = sorted(list(set(filter(lambda x: x.startswith('#'), dom_plans.comments.str.split(' ').agg(sum)))))
+  dconf = config[domain] if domain in config else dict()
+  if not tags:
+    continue
+  f.write(f'''<h2 id="{domain}">{domain}</h2>''')
+  for tag in tags:
+    tconf = dconf[tag[1:]] if tag[1:] in dconf else dict()
+    df = dom_plans[dom_plans.comments.str.contains(tag, case=False, na=False)]
+    df = df.assign(htmlstatus=pd.Series('<span class="' + df.status + '">' + df.experiment + '</span>', index=df.index))
+    df = df.assign(model_id=pd.Series(df.institute + '-' + df.rcm_name, index=df.index))
+    column_id = 'rcm_name' if collapse_institutions else 'model_id'
+    dom_plans_matrix = df.pivot_table(
+      index = ('driving_model', 'ensemble'),
+      columns = column_id,
+      values = 'htmlstatus',
+      aggfunc = lambda x: ' '.join(x.dropna())
+    )
+    dom_plans_matrix = pd.concat([  # Bring ERA5 to the top
+      dom_plans_matrix.query("driving_model == 'ERA5'"),
+      dom_plans_matrix.drop(('ERA5',''), axis=0, errors='ignore')
+    ], axis=0)
+    if collapse_institutions:
+      inst = df.drop_duplicates(subset=['institute','rcm_name']).pivot_table(
+        index = ('driving_model', 'ensemble'),
+        columns = 'rcm_name',
+        values = 'institute',
+        aggfunc = lambda x: ', '.join(x.dropna())
+      ).agg(lambda x: ', '.join(x.dropna()))
+      inst.name = ('','Institutes')
+      dom_plans_matrix = dom_plans_matrix.append(inst)
+      dom_plans_matrix = dom_plans_matrix.T.set_index([('','Institutes'),dom_plans_matrix.columns]).T
+      dom_plans_matrix.columns.names = ['Institution(s)','RCM']
+      title = tconf['title'] if 'title' in tconf else tag
+      descr = tconf['description'] if 'description' in tconf else ''
+      url = f'<p>URL: <a href="{tconf["url"]}">{tconf["url"]}</a>' if 'url' in tconf else ''
+    f.write(f'''<h3 id="{title}">{title}</h3>
+      <p> {descr}
+      {url}
+      <p style="font-size: smaller;"> Colour legend:
+        <span class="planned">planned</span>
+        <span class="running">running</span>
+        <span class="completed">completed</span>
+        <span class="published">published</span>
+      </p>
+    ''')
+    f.write(dom_plans_matrix.style
+       .set_properties(**{'font-size':'8pt', 'border':'1px lightgrey solid !important'})
+       .set_table_styles([d1,{
+          'selector': 'th',
+          'props': [('font-size', '8pt'),('border-style','solid'),('border-width','1px')]
+        }])
+       .render()
+       .replace('nan','')
+       .replace('historical','hist')
+    )
+f.write('</body></html>')
+f.close()
